409d0a792fbf054af6acdfe16aa722cbe8cd498a
Daniel Davidson <1096508+patefacio@users.noreply.github.com>
2024-04-25 06:50:09 -0500

    try out folds and remap close all other to move to other

--- init.lua
+++ init.lua
@@ -90,6 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.foldmethod = 'indent'
+vim.opt.wildmode = 'list:longest'
+
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
@@ -199,6 +202,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-w>o', '<C-w><C-w>', { desc = 'DBD-Remapped to move focus to other' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
